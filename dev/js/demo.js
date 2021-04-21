2229af868944742f7d716eaa16fd5468dd35a6f8
queeniechen79 <63672423+queeniechen79@users.noreply.github.com>
2021-04-20 20:15:24 -0400

    replaced files

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -1,20 +1,30 @@
 import $ from "jquery";
 import {gsap} from "gsap";
+//import the plugins
+import GSDevTools from "gsap/GSDevTools";
+
+//register plugins
+gsap.registerPlugin(GSDevTools);
 
 $(document).ready(function(){
 
-  //var tl = gsap.timeline();
+
+
+  GSDevTools.create();
+
+  var tl = gsap.timeline({id:"timeline"});
   var redBox = $('.red-box');
   var blueBox = $('.blue-box');
   var orangeBox = $('.orange-box');
   var boxArray = [redBox, blueBox, orangeBox];
 
   //Wrap Utility
-  //var color = gsap.utils.wrap([]);
+  var color = gsap.utils.wrap(["green","black", "blue"]);
+  var myX = gsap.utils.wrap([50,100, 150]);
 
   console.log('Page loaded');
 
-
+gsap.to(boxArray, {duration:1, id:"colorchange", backgroundColor:color, x:myX, stagger:0.5});
 
 
 
