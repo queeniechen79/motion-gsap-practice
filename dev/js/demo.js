4433ed207d95c9df64aa34da24fae8dad00b40ec
queeniechen79 <63672423+queeniechen79@users.noreply.github.com>
2021-05-04 19:03:10 -0400

    changes

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -20,15 +20,15 @@ $(document).ready(function(){
 
   //Wrap Utility
   var color = gsap.utils.wrap(["green","black", "blue"]);
-  var myX = gsap.utils.wrap([50,100, 150]);
+  var myX = gsap.utils.wrap([25,100, 200]);
 
   console.log('Page loaded');
-
-gsap.to(boxArray, {duration:1, id:"colorchange", backgroundColor:color, x:myX, stagger:0.5});
-
-
-
-
+  tl.to(blueBox ,{y:0});
+  tl.to(boxArray, {duration:1, id:"colorchange", backgroundColor:color, x:myX, stagger:0.5});
+  tl.to(blueBox ,{rotation:200, borderRadius:"100px"});
+  tl.to(blueBox ,{duration:0.75, scale:3, yoyo:true, repeat:1, ease:"back.out(6)"})
+  tl.to(blueBox ,{duration:0.5, y:2000})
+  tl.fromTo(blueBox ,{y:-2000}, {duration:0.5, y:0})
 
 
 
